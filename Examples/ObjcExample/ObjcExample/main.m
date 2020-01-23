0c212c11bd8ed65a74751f8e1e1fe05d261287ac
Alexander Schiewe <alexander.schiewe@img.ly>
2020-01-23 10:41:05 +0100

    Update examples for 10.7.1

--- Examples/ObjcExample/ObjcExample/main.m
+++ Examples/ObjcExample/ObjcExample/main.m
@@ -1,11 +1,11 @@
-//  This file is part of the VideoEditor Software Development Kit.
-//  Copyright (C) 2016-2019 img.ly GmbH <contact@img.ly>
+//  This file is part of an img.ly Software Development Kit.
+//  Copyright (C) 2016-2020 img.ly GmbH <contact@img.ly>
 //  All rights reserved.
 //  Redistribution and use in source and binary forms, without
 //  modification, are permitted provided that the following license agreement
 //  is approved and a legal/financial contract was signed by the user.
 //  The license agreement can be found under the following link:
-//  https://www.videoeditorsdk.com/LICENSE.txt
+//  https://www.photoeditorsdk.com/LICENSE.txt
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
